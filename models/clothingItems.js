fe9ed38a1c26bc95f2ce478676ca80e87ac00197
DominicMartinez <domvm.7@gmail.com>
2024-03-30 14:41:50 -0700

    created schemas for users and clothing items

---
+++ models/clothingItems.js
@@ -0,0 +1,47 @@
+// clothingItem.js
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const clothingItemSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  weather: {
+    type: String,
+    required: true,
+    enum: ["hot", "warm", "cold"],
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (value) {
+        return validator.isURL(value);
+      },
+      message: (props) => `${props.value} is not a valid URL for imageUrl.`,
+    },
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  likes: {
+    type: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    default: [],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("ClothingItem", clothingItemSchema);
