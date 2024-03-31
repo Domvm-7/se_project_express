98be41985ab87c037799e102651b24dfe6139bad
DominicMartinez <domvm.7@gmail.com>
2024-03-30 17:18:40 -0700

    Video 4 starting

---
+++ controllers/users.js
@@ -0,0 +1,10 @@
+const User = require("../models/user");
+
+const getUsers = (req, res) => {
+  User.find({})
+    .then((users) => res.send(users))
+    .catch((err) => {});
+  console.error(err);
+};
+
+module.exports = { getUsers };
