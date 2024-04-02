472576ea670c259ecf131b40479315849f9412f1
DominicMartinez <domvm.7@gmail.com>
2024-04-02 08:59:56 -0700

    app.js removed line via reviewer

--- app.js
+++ app.js
@@ -27,23 +27,6 @@ app.use(express.json());
 
 app.use("/", mainRouter);
 
-// Error Handling Middleware
-app.use((err, req, res, next) => {
-  console.error(err);
-  let statusCode = errors.SERVER_ERROR;
-  let message = "An error occurred on the server.";
-
-  if (err.name === "ValidationError" || err.name === "CastError") {
-    statusCode = errors.INVALID_DATA;
-    message = "Invalid data passed.";
-  } else if (err.name === "DocumentNotFoundError") {
-    statusCode = errors.NOT_FOUND;
-    message = "Resource not found.";
-  }
-
-  res.status(statusCode).json({ message });
-});
-
 app.listen(PORT, () => {
   console.log(`Server is running port ${PORT}`);
 });
