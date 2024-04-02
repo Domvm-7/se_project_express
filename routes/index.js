1771c44bc5b8ade835a8a95e75b2fce438e336b1
DominicMartinez <domvm.7@gmail.com>
2024-04-02 09:06:15 -0700

    fixed routes index.js per reviewer

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,18 @@
-const router = require("express").Router();
+// routes/index.js
+const express = require("express");
+const router = express.Router();
 
+// Importing user routes
 const userRouter = require("./users");
-
 router.use("/users", userRouter);
+
+// Importing item routes
+const itemRouter = require("./clothingItems");
+router.use("/items", itemRouter);
+
+// Middleware for handling unknown routes (404)
+router.use((req, res) => {
+  res.status(404).send("Route not found");
+});
 
 module.exports = router;
