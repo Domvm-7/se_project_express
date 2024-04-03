bfbb25c74d23172d193ef573377e9e838965cfbb
DominicMartinez <domvm.7@gmail.com>
2024-04-03 08:16:47 -0700

    working on test failures

--- controllers/clothingItems.js
+++ controllers/clothingItems.js
@@ -1,14 +1,21 @@
-const { DEFAULT, BAD_REQUEST, NOT_FOUND } = require("../utils/errors");
+// controllers/clothingItems.js
+
 const ClothingItem = require("../models/clothingItem");
+
+const DEFAULT = 500; // Added constant for default status code
 
 exports.getItems = async (req, res) => {
   try {
     const items = await ClothingItem.find({});
+
     return res.json(items);
   } catch (err) {
     console.error(err);
+
     return res
-      .status(DEFAULT)
+
+      .status(DEFAULT) // Using created constant for status code
+
       .json({ message: "An error occurred on the server." });
   }
 };
@@ -17,21 +24,32 @@ exports.createItem = async (req, res) => {
   try {
     const newItem = new ClothingItem({
       name: req.body.name,
+
       weather: req.body.weather,
+
       imageUrl: req.body.imageUrl,
+
       owner: req.user._id, // assuming temporary workaround is in place
     });
+
     const savedItem = await newItem.save();
+
     return res.status(201).json(savedItem);
   } catch (err) {
     if (err.name === "ValidationError") {
       return res
-        .status(BAD_REQUEST)
+
+        .status(400)
+
         .json({ message: "Invalid data passed to create item." });
     }
+
     console.error(err);
+
     return res
-      .status(DEFAULT)
+
+      .status(DEFAULT) // Using created constant for status code
+
       .json({ message: "An error occurred on the server." });
   }
 };
@@ -39,17 +57,23 @@ exports.createItem = async (req, res) => {
 exports.deleteItem = async (req, res) => {
   try {
     const deletedItem = await ClothingItem.findByIdAndRemove(req.params.itemId);
+
     if (!deletedItem) {
-      return res.status(NOT_FOUND).json({ message: "Item not found." });
+      return res.status(404).json({ message: "Item not found." });
     }
+
     return res.json({ message: "Item deleted." });
   } catch (err) {
     if (err.name === "CastError") {
-      return res.status(BAD_REQUEST).json({ message: "Invalid item ID." });
+      return res.status(400).json({ message: "Invalid item ID." });
     }
+
     console.error(err);
+
     return res
-      .status(DEFAULT)
+
+      .status(DEFAULT) // Using created constant for status code
+
       .json({ message: "An error occurred on the server." });
   }
 };
@@ -58,20 +82,28 @@ exports.likeItem = async (req, res) => {
   try {
     const updatedItem = await ClothingItem.findByIdAndUpdate(
       req.params.itemId,
+
       { $addToSet: { likes: req.user._id } },
+
       { new: true },
     );
+
     if (!updatedItem) {
-      return res.status(NOT_FOUND).json({ message: "Item not found." });
+      return res.status(404).json({ message: "Item not found." });
     }
+
     return res.json(updatedItem);
   } catch (err) {
     if (err.name === "CastError") {
-      return res.status(BAD_REQUEST).json({ message: "Invalid item ID." });
+      return res.status(400).json({ message: "Invalid item ID." });
     }
+
     console.error(err);
+
     return res
-      .status(DEFAULT)
+
+      .status(DEFAULT) // Using created constant for status code
+
       .json({ message: "An error occurred on the server." });
   }
 };
@@ -80,20 +112,28 @@ exports.dislikeItem = async (req, res) => {
   try {
     const updatedItem = await ClothingItem.findByIdAndUpdate(
       req.params.itemId,
+
       { $pull: { likes: req.user._id } },
+
       { new: true },
     );
+
     if (!updatedItem) {
-      return res.status(NOT_FOUND).json({ message: "Item not found." });
+      return res.status(404).json({ message: "Item not found." });
     }
+
     return res.json(updatedItem);
   } catch (err) {
     if (err.name === "CastError") {
-      return res.status(BAD_REQUEST).json({ message: "Invalid item ID." });
+      return res.status(400).json({ message: "Invalid item ID." });
     }
+
     console.error(err);
+
     return res
-      .status(DEFAULT)
+
+      .status(DEFAULT) // Using created constant for status code
+
       .json({ message: "An error occurred on the server." });
   }
 };
--- controllers/users.js
+++ controllers/users.js
@@ -1,20 +1,25 @@
 const User = require("../models/user");
 
-const {
-  STATUS_DEFAULT,
-  STATUS_NOT_FOUND,
-  STATUS_BAD_REQUEST,
-  STATUS_CREATED,
-} = require("../utils/errors");
+const STATUS_DEFAULT = 500;
+
+const STATUS_NOT_FOUND = 404;
+
+const STATUS_BAD_REQUEST = 400;
+
+const STATUS_CREATED = 201;
 
 exports.getUsers = async (req, res) => {
   try {
     const users = await User.find({});
+
     res.json(users);
   } catch (err) {
     console.error(err);
+
     res
+
       .status(STATUS_DEFAULT)
+
       .json({ message: "An error occurred on the server." });
   }
 };
@@ -22,20 +27,26 @@ exports.getUsers = async (req, res) => {
 exports.getUser = async (req, res) => {
   try {
     const user = await User.findById(req.params.userId);
+
     if (!user) {
       return res.status(STATUS_NOT_FOUND).json({ message: "User not found." });
     }
+
     res.json(user);
   } catch (err) {
     if (err.name === "CastError") {
       res.status(STATUS_BAD_REQUEST).json({ message: "Invalid user ID." });
     } else {
       console.error(err);
+
       res
+
         .status(STATUS_DEFAULT)
+
         .json({ message: "An error occurred on the server." });
     }
   }
+
   return null;
 };
 
@@ -43,21 +54,30 @@ exports.createUser = async (req, res) => {
   try {
     const newUser = new User({
       name: req.body.name,
+
       avatar: req.body.avatar,
     });
+
     const savedUser = await newUser.save();
+
     res.status(STATUS_CREATED).json(savedUser);
   } catch (err) {
     if (err.name === "ValidationError") {
       res
+
         .status(STATUS_BAD_REQUEST)
+
         .json({ message: "Invalid data passed to create user." });
     } else {
       console.error(err);
+
       res
+
         .status(STATUS_DEFAULT)
+
         .json({ message: "An error occurred on the server." });
     }
   }
+
   return null;
 };
