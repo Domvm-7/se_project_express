967788c874a356aa650eacd5465945697a9315c0
DominicMartinez <domvm.7@gmail.com>
2024-04-03 08:00:45 -0700

    working on test failures 4

--- models/clothingItem.js
+++ models/clothingItem.js
@@ -1,139 +1,54 @@
-// controllers/clothingItems.js
+// models/clothingItem.js
+const mongoose = require("mongoose");
 
-const ClothingItem = require("../models/clothingItem");
+const validator = require("validator");
 
-const DEFAULT = 500; // Added constant for default status code
+const clothingItemSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
 
-exports.getItems = async (req, res) => {
-  try {
-    const items = await ClothingItem.find({});
+  weather: {
+    type: String,
+    required: true,
+    enum: ["hot", "warm", "cold"],
+  },
 
-    return res.json(items);
-  } catch (err) {
-    console.error(err);
+  imageUrl: {
+    type: String,
+    required: true,
+    validate: {
+      validator(value) {
+        return validator.isURL(value);
+      },
+      message(props) {
+        return `${props.value} is not a valid URL for imageUrl.`;
+      },
+    },
+  },
 
-    return res
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  likes: {
+    type: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
 
-      .status(DEFAULT) // Using created constant for status code
+    default: [],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-      .json({ message: "An error occurred on the server." });
-  }
-};
-
-exports.createItem = async (req, res) => {
-  try {
-    const newItem = new ClothingItem({
-      name: req.body.name,
-
-      weather: req.body.weather,
-
-      imageUrl: req.body.imageUrl,
-
-      owner: req.user._id, // assuming temporary workaround is in place
-    });
-
-    const savedItem = await newItem.save();
-
-    return res.status(201).json(savedItem);
-  } catch (err) {
-    if (err.name === "ValidationError") {
-      return res
-
-        .status(400)
-
-        .json({ message: "Invalid data passed to create item." });
-    }
-
-    console.error(err);
-
-    return res
-
-      .status(DEFAULT) // Using created constant for status code
-
-      .json({ message: "An error occurred on the server." });
-  }
-};
-
-exports.deleteItem = async (req, res) => {
-  try {
-    const deletedItem = await ClothingItem.findByIdAndRemove(req.params.itemId);
-
-    if (!deletedItem) {
-      return res.status(404).json({ message: "Item not found." });
-    }
-
-    return res.json({ message: "Item deleted." });
-  } catch (err) {
-    if (err.name === "CastError") {
-      return res.status(400).json({ message: "Invalid item ID." });
-    }
-
-    console.error(err);
-
-    return res
-
-      .status(DEFAULT) // Using created constant for status code
-
-      .json({ message: "An error occurred on the server." });
-  }
-};
-
-exports.likeItem = async (req, res) => {
-  try {
-    const updatedItem = await ClothingItem.findByIdAndUpdate(
-      req.params.itemId,
-
-      { $addToSet: { likes: req.user._id } },
-
-      { new: true },
-    );
-
-    if (!updatedItem) {
-      return res.status(404).json({ message: "Item not found." });
-    }
-
-    return res.json(updatedItem);
-  } catch (err) {
-    if (err.name === "CastError") {
-      return res.status(400).json({ message: "Invalid item ID." });
-    }
-
-    console.error(err);
-
-    return res
-
-      .status(DEFAULT) // Using created constant for status code
-
-      .json({ message: "An error occurred on the server." });
-  }
-};
-
-exports.dislikeItem = async (req, res) => {
-  try {
-    const updatedItem = await ClothingItem.findByIdAndUpdate(
-      req.params.itemId,
-
-      { $pull: { likes: req.user._id } },
-
-      { new: true },
-    );
-
-    if (!updatedItem) {
-      return res.status(404).json({ message: "Item not found." });
-    }
-
-    return res.json(updatedItem);
-  } catch (err) {
-    if (err.name === "CastError") {
-      return res.status(400).json({ message: "Invalid item ID." });
-    }
-
-    console.error(err);
-
-    return res
-
-      .status(DEFAULT) // Using created constant for status code
-
-      .json({ message: "An error occurred on the server." });
-  }
-};
+module.exports = mongoose.model("ClothingItem", clothingItemSchema);
